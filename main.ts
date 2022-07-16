de9d97de72d958b080ce9c97a6c3e049388030dd
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2022-07-16 21:18:03 +0200

    stable version with appearances adjusted

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
+/******************************************************************
+ * Functions for optical encoder on smart car.
+ * Author: Rapha Pretorius
+ * Email: raphapretorius@gmail.com
+******************************************************************/
+
 
 //% weight=40 color=#226025 icon="\uf110" block="Encoder"
-namespace encoder {
-    
+//% groups=['Initial Setup', 'Functions', 'Variables - Counters', 'Variables - Distance']
+namespace Encoder {
+    // Set Global Variables.
     let wheelDiameter : number
     let stateSaved1: number
     let stateSaved2 : number
@@ -14,8 +21,14 @@ namespace encoder {
     let encoderOnePin : number
     let diskSlots : number
 
+    /*
+    *Input values for your specific encoder disk and motor wheels.
+    */
+    //% group="Initial Setup"
     //% weight=38
-    //% block="Wheel Diameter %wheelDia Number of disk slots %slots Encoder 1 to pin %e1pin Encoder 2 to pin %e2pin"
+    //% blockGap=8
+    //% blockId=set_your_values_here
+    //% block="Wheel Diameter (mm) %wheelDia Number of disk slots %slots Encoder 1 to pin %e1pin Encoder 2 to pin %e2pin"
     export function setValues(wheelDia:number,slots:number,e1pin:number,e2pin:number):void {
         wheelDiameter = wheelDia
         diskSlots = slots
@@ -29,18 +42,21 @@ namespace encoder {
         counter1 = 0
         counter2 = 0
     }
-    
-    // //% weight=25 block = "distance (mm)"
-    //export function mmMovedPerSlot():number{
-    //    return(mmPerSlot);
-    //}
+    //% group="Functions"
+    //% weight=36 
+    //% blockGap=8
+    //% block="reset all encoder counters"
+    export function resetAllEncoderCounters(): void {
+        counter1 = 0
+        counter2 = 0
+        stateSaved1 = pins.digitalReadPin(encoderOnePin)
+        stateSaved2 = pins.digitalReadPin(encoderTwoPin)
+    }
 
-    // //% weight=24 block = "degrees per slot"
-    //export function degreePerSlot(): number {
-    //    return (degPerSlot);
-    //}
-
-    //% weight=29 block = "encoder 1 counter"
+    //% group="Variables - Counters"
+    //% weight=29 
+    //% blockGap=8
+    //% block="encoder 1 (slots)"
     export function encoder1Count():number{
         if(stateSaved1 != pins.digitalReadPin(encoderOnePin)){
             if (pins.digitalReadPin(encoderOnePin) == 1){
@@ -50,7 +66,10 @@ namespace encoder {
         }
         return(counter1);
     }
-    //% weight=28 block="encoder 2 counter"
+    //% group="Variables - Counters"
+    //% weight=28 
+    //% blockGap=40
+    //% block="encoder 2 (slots)"
     export function encoder2Count(): number {
         if (stateSaved2 != pins.digitalReadPin(encoderTwoPin)) {
             if (pins.digitalReadPin(encoderTwoPin) == 1) {
@@ -61,18 +80,17 @@ namespace encoder {
         return (counter2);
     }
 
-    //% weight=36 block="reset all encoder counters"
-    export function resetAllEncoderCounters():void {
-        counter1 = 0
-        counter2 = 0
-        stateSaved1 = pins.digitalReadPin(encoderOnePin)
-        stateSaved2 = pins.digitalReadPin(encoderTwoPin)
-    }
-    //% weight=19 block="encoder 1 moved (mm)"
+    //% group="Variables - Distance"
+    //% weight=19 
+    //% blockGap=8
+    //% block="wheel 1 (mm)"
     export function mmMovedOne():number{
         return(mmPerSlot*counter1)
     }
-    //% weight=18 block="encoder 2 moved (mm)"
+    //% group="Variables - Distance"
+    //% weight=18 
+    //% blockGap=40
+    //% block="wheel 2 (mm)"
     export function mmMovedTwo():number{
         return(mmPerSlot*counter2)
     }
