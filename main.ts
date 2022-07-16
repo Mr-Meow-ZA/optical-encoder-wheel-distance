b9fd191f58466bf0cebf53bab9d93988335b5acb
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2022-07-16 19:35:43 +0200

    testing first blocks for this

--- main.ts
+++ main.ts
@@ -1,52 +1,16 @@
-function setValues () {
-    wheelDiameter = 66.1
-    diskSlots = 20
-    encoderOnePin = 0
-    encoderTwoPin = 9
-    degPerSlot = 360 / diskSlots
-    wheelCircumference = wheelDiameter * 3.14159
-    mmPerSlot = wheelCircumference / 360 * degPerSlot
-}
-// checks that state has changed before incrementing, function returns cm travelled.
-function encoder1 () {
-    if (stateSaved1 != pins.digitalReadPin(DigitalPin.P0)) {
-        if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-            counter1 += pins.digitalReadPin(DigitalPin.P0)
-        }
-        stateSaved1 = pins.digitalReadPin(DigitalPin.P0)
+
+
+namespace encoder {
+    
+
+
+    //%block="Wheel Diameter %wheelDiameter \n Number of disk slots %diskSlots \n Encoder 1 pin %encoderOnePin \n Encoder 2 pin %encoderTwoPin"
+
+    export function setValues(wheelDiameter:number,diskSlots:number,encoderOnePin:number,encoderTwoPin:number) {
+        let degPerSlot = 360 / diskSlots
+        let wheelCircumference = wheelDiameter * 3.14159
+        let mmPerSlot = wheelCircumference / 360 * degPerSlot
+        return(encoderOnePin,encoderTwoPin,mmPerSlot);
     }
-    return counter1
+    
 }
-function clearEncoders () {
-    counter1 = 0
-    counter2 = 0
-    stateSaved1 = pins.digitalReadPin(DigitalPin.P0)
-    stateSaved2 = pins.digitalReadPin(DigitalPin.P9)
-}
-function encoder2 () {
-    if (stateSaved2 != pins.digitalReadPin(DigitalPin.P9)) {
-        if (pins.digitalReadPin(DigitalPin.P9) == 1) {
-            counter2 += pins.digitalReadPin(DigitalPin.P9)
-        }
-        stateSaved2 = pins.digitalReadPin(DigitalPin.P9)
-    }
-    return counter2
-}
-function stepsToMilimeters (count: number) {
-    return 10.38 * count
-}
-let stateSaved2 = 0
-let counter2 = 0
-let counter1 = 0
-let stateSaved1 = 0
-let mmPerSlot = 0
-let wheelCircumference = 0
-let degPerSlot = 0
-let encoderTwoPin = 0
-let encoderOnePin = 0
-let diskSlots = 0
-let wheelDiameter = 0
-clearEncoders()
-basic.forever(function () {
-	
-})
